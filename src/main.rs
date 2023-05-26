dc72353ab111a0a2e0ec675ac18618cf33a37eb1
Luke Thompson <endeavour9@gmail.com>
2023-05-26 16:11:25 +1000

    add the market proeprty

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,34 @@
 
 #[cfg(test)] mod tests;
 
+#[get("/")]
+fn home() -> String {
+    format!("<html>
+    <head>
+        <title>Barcode API</title>
+    </head>
+    <body>
+        <h1>Barcode API</h1>
+        <p>Use the following URL to get a barcode:</p>
+        <p><a href=\"https://barcode-api.herokuapp.com/barcode/1234\">https://barcode-api.herokuapp.com/barcode/1234</a></p>
+    </body>
+</html>
+")
+}
+
 #[get("/barcode/<barcode>")]
-fn hello(barcode: String) -> String {
+fn barcode(barcode: String) -> String {
     format!("{{\"barcode\": \"{barcode}\", \
     \"name\": \"Grape Test\", \
     \"fruit\": \"grapes\", \
     \"process\": \"main\", \
     \"standard\": \"premium\", \
+    \"market\": \"Harris Farms\", \
     \"image\": \"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png\"\
     }}\
 ")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![barcode, home]).launch();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -2,8 +2,8 @@ use super::rocket;
 use rocket::local::Client;
 
 #[test]
-fn hello_world() {
-    let rocket = rocket::ignite().mount("/", routes![super::hello]);
+fn barcode() {
+    let rocket = rocket::ignite().mount("/", routes![super::barcode]);
     let client = Client::new(rocket).unwrap();
     let mut response = client.get("/barcode/1234").dispatch();
     assert_eq!(response.body_string(), Some("{\"barcode\": \"1234\", \
@@ -11,6 +11,7 @@ fn hello_world() {
     \"fruit\": \"grapes\", \
     \"process\": \"main\", \
     \"standard\": \"premium\", \
+    \"market\": \"Harris Farms\", \
     \"image\": \"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png\"\
     }\
 ".into()));
